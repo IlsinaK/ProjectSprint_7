04854c51a393922b2b8aece4e0a219d52153daa1
Ilsina Komarova <ilsinchik@list.ru>
2024-12-18 10:20:47 +0300

    add model file

---
+++ src/main/java/model/CourierDataLombok.java
@@ -0,0 +1,23 @@
+package model;
+
+import io.qameta.allure.internal.shadowed.jackson.annotation.JsonIgnoreProperties;
+import io.qameta.allure.internal.shadowed.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CourierDataLombok {
+    private String login;
+    private String password;
+    private String firstName;
+    private Integer id;
+
+    // Конструктор для создания курьера с логином, паролем и именем
+    public CourierDataLombok(String login, String password, String firstName) {
+        this.login = login;
+        this.password = password;
+        this.firstName = firstName;
+    }
